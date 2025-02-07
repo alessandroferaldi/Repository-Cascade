ed64b643bc0f4e24dfd9351de079c936eadeb03c
alessandroferaldi <79195344+alessandroferaldi@users.noreply.github.com>
2025-02-07 14:38:12 +0100

    Update Data Processing.R

--- R/Data Processing.R
+++ R/Data Processing.R
@@ -7,19 +7,19 @@ require(tidyverse)
 ######## Import Data and Pre processing ########
 
 ### Import ISO3 list ####
-iso3<-read.csv2(file="~/Desktop/Data/ISO3.csv")
+iso3<-read.csv2(file="ISO3.csv")
 
 ### Import World Bank Income groups 2023 ####
-inc_grp<-read.csv(file="~/Desktop/Data/WB_income2023.csv")
+inc_grp<-read.csv(file="WB_income2023.csv")
 
 # Set Upper-middle Income Group for Venezuela (as in 2022), since 2023 is missing
 inc_grp$wbinc2023<-ifelse(inc_grp$ISO=="VEN","UM",inc_grp$wbinc2023)   
 
 ### Import World Bank Regions ####
-wbreg<-read.csv(file="~/Desktop/Data/WB_region.csv")          
+wbreg<-read.csv(file="WB_region.csv")          
 
 ### Import Risk Factors and include ISO3, World Bank income group and region (Adjust location names) ####
-risk<-read.csv2(file="~/Desktop/Data/Risk_factors_2019.csv") 
+risk<-read.csv2(file="Risk_factors_2019.csv") 
 risk$Location<-ifelse(risk$Location=="Türkiye","Turkey",risk$Location)
 risk$Location<-ifelse(risk$Location=="Côte d'Ivoire","Cte d'Ivoire",risk$Location)
 risk<-merge(iso3,risk,by.x="location",by.y="Location",all.x = T)
@@ -27,7 +27,7 @@ risk<-merge(risk,inc_grp,by.x="iso",by.y="ISO",all.x = T)
 risk<-merge(risk,wbreg,by.x="iso",by.y="ISO3",all.x = T)
 
 ### Import Prevalence and include ISO3, World Bank income group and region (Adjust location names) ####
-prev<-read.csv(file="~/Desktop/Data/Cause_prevalence.csv")
+prev<-read.csv(file="Cause_prevalence.csv")
 prev$Location<-ifelse(prev$Location=="Türkiye","Turkey",prev$Location)
 prev$Location<-ifelse(prev$Location=="Côte d'Ivoire","Cte d'Ivoire",prev$Location)
 prev<-merge(iso3,prev,by.x="location",by.y="Location",all.y=T)
@@ -35,7 +35,7 @@ prev<-merge(prev,inc_grp,by.x="iso",by.y="ISO",all.x=T)
 prev<-merge(prev,wbreg,by.x="iso",by.y="ISO3",all.x=T)
 
 ### Import Death Rates and include ISO3, World Bank income group and region (Adjust location names) ####
-mort<-read.csv(file="~/Desktop/Data/Cause_mortality.csv") 
+mort<-read.csv(file="Cause_mortality.csv") 
 mort$Location<-ifelse(mort$Location=="Türkiye","Turkey",mort$Location)
 mort$Location<-ifelse(mort$Location=="Côte d'Ivoire","Cte d'Ivoire",mort$Location)
 mort<-merge(iso3,mort,by.x="location",by.y="Location",all.y=T)
@@ -43,12 +43,12 @@ mort<-merge(mort,inc_grp,by.x="iso",by.y="ISO",all.x=T)
 mort<-merge(mort,wbreg,by.x="iso",by.y="ISO3",all.x=T)
 
 ### Hypertension Cascade (include ISO3, World Bank income group and region) ####
-hyp<-read.csv2(file="~/Desktop/Data/Hypertension_cascade.csv")
+hyp<-read.csv2(file="Hypertension_cascade.csv")
 hyp<-merge(hyp,inc_grp,by.x="ISO",by.y="ISO",all.x = T)
 hyp<-merge(hyp,wbreg,by.x="ISO",by.y="ISO3",all.x = T)
 
 ### Importn Diabetes mellitus Cascade (include ISO3, World Bank income group and region) ####
-dat_diab<-read.csv2(file="~/Desktop/Data/Diabetes_cascade.csv",sep="\t") ### with income groups
+dat_diab<-read.csv2(file="Diabetes_cascade.csv",sep="\t") ### with income groups
 
 ### Remove tested
 dat_diab1<- dat_diab %>% filter(Step!="tested")
@@ -74,7 +74,7 @@ dat_diab_age<-dat_diab_age %>% filter(Age!="15-29")
 diab<-dat_diab_age
 
 ### Import ISO3 and include World Bank income group and region
-iso3_diab<-read.csv2(file="~/Desktop/Data/ISO3_diab.csv")
+iso3_diab<-read.csv2(file="ISO3_diab.csv")
 diab<-merge(iso3_diab,diab,by.x="location",by.y="Country")
 diab<-merge(diab,inc_grp,by.x="iso",by.y="ISO",all.x = T)
 diab<-merge(diab,wbreg,by.x="iso",by.y="ISO3",all.x = T)
@@ -104,7 +104,7 @@ for(i in 1:nrow(diab)){
 diab$iso3<-diab$iso
 
 ### Import HIV/AIDS Cascade and include ISO3, World Bank income group and region ##### 
-HIV_AIDS<-read.csv2(file="~/Desktop/Data/HIVAIDS_cascade.csv")
+HIV_AIDS<-read.csv2(file="HIVAIDS_cascade.csv")
 
 ### Adjust country names
 HIV_AIDS$Country<-ifelse(HIV_AIDS$Country=="Cape Verde","Cabo Verde",HIV_AIDS$Country)
